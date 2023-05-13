0502bf0d0aad3e01341788fad0b0975de74b72dc
Mukul <mukulmotakatla7@gmail.com>
2023-05-13 09:43:36 +0530

    points data separated

--- lib/Pages/points.dart
+++ lib/Pages/points.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
 import 'package:http/http.dart' as http;
@@ -8,17 +11,51 @@ class Point extends StatefulWidget {
   _PointState createState() => _PointState();
 }
 class _PointState extends State<Point> {
+
   List<List<dynamic>> _rows = [];
   Map<DateTime, double> _goodPointCounts = {};
   Map<DateTime, double> _badPointCounts = {};
-  @override
-  void initState() {
-    super.initState();
-    _loadCSV();
+
+  String url = '';
+  User? user = FirebaseAuth.instance.currentUser!;
+  late DatabaseReference _databaseReference;
+  late final String documentId;
+  List<String> docIDs = [];
+
+  Future<void> getDocID() async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: user?.email)
+        .get();
+
+    setState(() {
+    docIDs = querySnapshot.docs.map((doc) => doc.id).toList();
+      Map<String, dynamic>? userData = querySnapshot.docs[0].data() as Map<String, dynamic>?;
+      url = (userData?['url'] as String?)!;
+    });
   }
+
+  @override
+    void initState() {
+    super.initState();
+    getDocID().then((value) {
+      if (docIDs.isNotEmpty) {
+        documentId = docIDs[0];
+        _databaseReference = FirebaseDatabase.instance.reference().child('/users/$documentId');
+        _databaseReference.onValue.listen((event) {
+        });
+        _loadCSV();
+      } else {
+        // handle error: no document found for the user
+      }
+    }).catchError((error) {
+      // handle error: failed to fetch the document ID
+    });
+  }
+  
   Future<void> _loadCSV() async {
     try {
-      final response = await http.get(Uri.parse('https://drive.google.com/uc?id=17_UgYlbGAoKIHtRNgAF0wPkXleq0ExGf&export=download'));
+      final response = await http.get(Uri.parse(url));
       final List<List<dynamic>> csvTable = CsvToListConverter().convert(response.body);
       setState(() {
         _rows = csvTable;
